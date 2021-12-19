d26ae4841612e6a4a5e852766e5d616d18cc937c
rassi0429 <taitokokoa+rassi@gmail.com>
2021-12-19 23:21:45 +0900

    feat: Spawn Session Orb

--- MessageCopy/MessageCopy.cs
+++ MessageCopy/MessageCopy.cs
@@ -30,20 +30,65 @@ namespace MessageCopy
         {
             static void Postfix(UIBuilder ___messagesUi, FriendsDialog __instance, ref Image __result, Message message)
             {
-                if (message.MessageType != CloudX.Shared.MessageType.Text) return;
-                List<Slot> child = ___messagesUi.Current.GetAllChildren();
-                foreach (Slot c in child)
+                if (message.MessageType == CloudX.Shared.MessageType.Text)
                 {
-                    if (c.GetComponent<Text>() != null)
+                    List<Slot> child = ___messagesUi.Current.GetAllChildren();
+                    foreach (Slot c in child)
                     {
-                        var text = c.GetComponent<Text>();
-                        string msg = text.Content;
-                        text.Destroy();
-                        var ui = new UIBuilder(c);
-                        ui.Button(msg, new color(1, 1, 1, 0)).LocalPressed += (IButton btn, ButtonEventData _) => { btn.World.InputInterface.Clipboard.SetText(btn.LabelText); };
-                        var btnText = c.GetComponentInChildren<Text>();
-                        btnText.Align = message.IsSent ? Alignment.MiddleRight : Alignment.MiddleLeft;
+                        if (c.GetComponent<Text>() != null)
+                        {
+                            var text = c.GetComponent<Text>();
+                            string msg = text.Content;
+                            Msg(msg);
+                            text.Destroy();
+                            var ui = new UIBuilder(c);
+                            ui.Button(msg, new color(1, 1, 1, 0)).LocalPressed += (IButton btn, ButtonEventData _) => { btn.World.InputInterface.Clipboard.SetText(btn.LabelText); };
+                            var btnText = c.GetComponentInChildren<Text>();
+                            btnText.Align = message.IsSent ? Alignment.MiddleRight : Alignment.MiddleLeft;
+                        }
                     }
+                }
+                else if (message.MessageType == CloudX.Shared.MessageType.SessionInvite)
+                {
+                    Msg("Invite");
+                    List<Slot> child = ___messagesUi.Current.GetAllChildren();
+                    foreach (Slot c in child)
+                    {
+                        if (c.GetComponent<Text>() != null)
+                        {
+                            var text = c.GetComponent<Text>().Content;
+                            if (text == "Join")
+                            {
+                                Slot d = c.Parent.Duplicate();
+                                var t = d.GetComponent<RectTransform>();
+                                t.AnchorMin.Value = new float2(0.4f, 0f);
+                                t.AnchorMax.Value = new float2(0.69f, 1f);
+                                var btn = d.GetComponent<Button>();
+                                var image = d.GetComponent<Image>();
+                                btn.Destroy();
+                                image.Destroy();
+                                var newImage = d.AttachComponent<Image>();
+                                var newBtn = d.AttachComponent<Button>();
+                                newBtn.LocalPressed += (IButton b, ButtonEventData _) =>
+                                {
+                                    SessionInfo sessionInfo = message.ExtractContent<SessionInfo>();
+                                    World world = __instance.LocalUser.World.WorldManager.FocusedWorld;
+                                    world.RunSynchronously((Action)(() =>
+                                    {
+                                        Slot slot = world.RootSlot.LocalUserSpace.AddSlot("World Orb");
+                                        WorldOrb worldOrb = slot.AttachComponent<WorldOrb>();
+                                        worldOrb.ActiveSessionURLs = sessionInfo.GetSessionURLs();
+                                        worldOrb.ActiveUsers.Value = sessionInfo.JoinedUsers;
+                                        worldOrb.WorldName = sessionInfo.Name;
+                                        slot.PositionInFrontOfUser();
+                                    }));
+                                };
+                                d.GetComponentInChildren<Text>().Content.Value = "Orb";
+                            }
+                        }
+                    }
+
+
                 }
             }
         }
