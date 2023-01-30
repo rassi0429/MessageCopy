33fbd45519b4cea6613e30d8e8d7eccca46a27a9
art0007i <art0007i@gmail.com>
2023-01-30 18:01:24 +0100

    remake the mod

--- MessageCopy/MessageCopy.cs
+++ MessageCopy/MessageCopy.cs
@@ -4,10 +4,9 @@ using System;
 using System.Reflection;
 using FrooxEngine;
 using BaseX;
-using CloudX.Shared;
-using CodeX;
 using FrooxEngine.UIX;
 using System.Collections.Generic;
+using CloudX.Shared;
 
 namespace MessageCopy
 {
@@ -15,12 +14,17 @@ namespace MessageCopy
     {
         public override string Name => "MessageCopy";
         public override string Author => "kka429";
-        public override string Version => "1.0.0";
+        public override string Version => "2.0.0";
         public override string Link => "https://github.com/rassi0429/MessageCopy"; // this line is optional and can be omitted
+
+        [AutoRegisterConfigKey]
+        public static ModConfigurationKey<bool> KEY_TIMESTAMPS = new("always_timestamp", "If true timestamps will be displayed on every message.", () => true);
+        internal static ModConfiguration config;
 
         public override void OnEngineInit()
         {
-            Harmony harmony = new Harmony("dev.kokoa.messagecopy");
+            config = GetConfiguration();
+            var harmony = new Harmony("dev.kokoa.messagecopy");
             harmony.PatchAll();
         }
 
@@ -28,23 +32,45 @@ namespace MessageCopy
         [HarmonyPatch("AddMessage")]
         class MessageCopyPatch
         {
-            static void Postfix(UIBuilder ___messagesUi, FriendsDialog __instance, ref Image __result, Message message)
+            static void Postfix(UIBuilder ___messagesUi, Image __result, Message message)
             {
-                if (message.MessageType != CloudX.Shared.MessageType.Text) return;
-                List<Slot> child = ___messagesUi.Current.GetAllChildren();
-                foreach (Slot c in child)
+                string toCopy;
+                switch (message.MessageType)
                 {
-                    if (c.GetComponent<Text>() != null)
-                    {
-                        var text = c.GetComponent<Text>();
-                        string msg = text.Content;
-                        text.Destroy();
-                        var ui = new UIBuilder(c);
-                        ui.Button(msg, new color(1, 1, 1, 0)).LocalPressed += (IButton btn, ButtonEventData _) => { btn.World.InputInterface.Clipboard.SetText(btn.LabelText); };
-                        var btnText = c.GetComponentInChildren<Text>();
-                        btnText.Align = message.IsSent ? Alignment.MiddleRight : Alignment.MiddleLeft;
-                    }
+                    case CloudX.Shared.MessageType.Text:
+                        toCopy = message.Content; break;
+                    case CloudX.Shared.MessageType.Sound:
+                    case CloudX.Shared.MessageType.Object:
+                        toCopy = message.ExtractContent<CloudX.Shared.Record>().AssetURI; break;
+                    case CloudX.Shared.MessageType.SessionInvite:
+                        // should this copy session id or first uri?
+                        toCopy = message.ExtractContent<SessionInfo>().SessionId; break;
+                    default:
+                        // CreditTransfer and SugarCubes cannot be copied (what would you even copy)
+                        return;
+                        
                 }
+                Slot cpyRoot;
+                if (___messagesUi.Current[0][0].Name == "Panel") cpyRoot = ___messagesUi.Current[0][0][0][0].AddSlot("CopyBtn");
+                else cpyRoot = ___messagesUi.Current[0].AddSlot("CopyBtn");
+                var rect = cpyRoot.AttachComponent<RectTransform>();
+                rect.AnchorMin.Value = new float2(1, 1);
+                rect.OffsetMin.Value = new float2(-12, -12);
+                cpyRoot.AttachComponent<Image>().Tint.Value = color.White.SetA(0.75f);
+                cpyRoot.AttachComponent<Button>().LocalPressed += (btn, data) => { cpyRoot.Engine.InputInterface.Clipboard.SetText(toCopy); };
+            }
+        }
+
+        // works a lot better with thisa
+        [HarmonyPatch(typeof(FriendsDialog))]
+        [HarmonyPatch("ShouldAddTimestamp")]
+        class TimestampPatch
+        {
+            public static bool Prefix(ref bool __result)
+            {
+                if (!config.GetValue(KEY_TIMESTAMPS)) return true;
+                __result = true;
+                return false;
             }
         }
     }
